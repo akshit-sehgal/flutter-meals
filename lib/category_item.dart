53492429e9a1822e34f0220bb9800f87c1d00b45
Akshit Sehgal <akshitsehgal95@gmail.com>
2019-11-22 18:42:11 +0530

    Customising AppBar and Navigating to Category Meals Screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
   CategoryItem({@required this.title, @required this.color});
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen();
+        }
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.5),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {
+        _selectCategory(context);
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.5),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,27 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontFamily: 'Raleway',
+                  fontSize: 20
+                ),
+              ),
+        ),
         textTheme:
             ThemeData.light().textTheme.apply(fontFamily: 'Raleway').copyWith(
-                body1: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
+                  body1: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 1),
+                  ),
+                  body2: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 1),
+                  ),
+                  title: TextStyle(
+                      fontFamily: 'RobotoCondensed',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
                 ),
-                body2: TextStyle(
-                  color: Color.fromRGBO(20, 51, 51, 1),
-                ),
-                title: TextStyle(
-                  fontFamily: 'RobotoCondensed',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                )),
       ),
       home: CategoriesScreen(),
     );
